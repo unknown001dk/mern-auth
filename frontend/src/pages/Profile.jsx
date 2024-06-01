67adc33a731ee91f70683bb24cc754b0d9738230
Dineshkumar <117102544+unknown001dk@users.noreply.github.com>
2024-06-01 07:21:20 +0530

    complete image upload functionality

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,15 +1,65 @@
 import {useSelector} from 'react-redux'
+import { useRef, useState, useEffect } from 'react'
+import { getStorage, uploadBytesResumable, ref, getDownloadURL } from 'firebase/storage'
+import app from '../firebase'
 
 function Profile() {
+  const fileRef = useRef(null);
+  const [image, setImage] = useState(undefined);
+  const [imagePercent, setImagePercent] = useState(0);
+  // console.log(imagePercent);
+  const [imageError, setImageError] = useState(false);
+  const [formData, setFormData] = useState({
+  });
+  // console.log(formData);
   const {currentUser} = useSelector(state => state.user);
+
+  useEffect(() => {
+    if (image) {
+      handleFileUpload(image);
+    }
+  }, [image]);
+
+  const handleFileUpload = async (image) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + image.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, image);
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImagePercent(Math.round(progress));
+        // console.log('upload is ' + progress + '% done');
+      });
+      (error) => {
+        setImageError(true);
+      }
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref)
+          .then((downloadURL) => {
+            setFormData({...formData, profilePicture: downloadURL});
+          })
+      }
+    console.log(image);
+  };
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
       <form className='flex flex-col gap-4'>
+        <input type="file" ref={fileRef} hidden accept='image/*' onChange={(e) => setImage(e.target.files[0])} />
         <img
-          src={currentUser.profilePicture}
+          onClick={() => fileRef.current.click()}
+          src={formData.profilePicture || currentUser.profilePicture}
           alt='profile-img'
           className='h-24 w-24 self-center curser-pointer rounded-full object-cover mt-2' />
+        <p className='text-sm self-center'>
+          {imageError ?
+            (<span className='text-red-700'>Error uploading on image (File size must be less than 2 MB)</span>) : imagePercent > 0 && imagePercent < 100 ?
+            (<span className='text-slate-700'>{`Uploading: ${imagePercent} % `}</span>) : imagePercent === 100 ?
+            (<span className='text-green-700'>Image Upload succesfully</span>) : ''}
+        </p>
         <input
           defaultValue={currentUser.username}
           id='username'
